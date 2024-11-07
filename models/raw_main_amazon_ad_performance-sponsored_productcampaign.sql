9c0ec6c007006c590dc09ae891e5105736fccc05
Miguelo Sana <miguelo@incubeta.com>
2024-11-07 15:44:07 +0100

    remove old columns

--- models/raw_main_amazon_ad_performance-sponsored_productcampaign.sql
+++ models/raw_main_amazon_ad_performance-sponsored_productcampaign.sql
@@ -26,22 +26,21 @@ profile as (
 	select * from {{ source('dbt_amazon_ads', 'profile')}}
 ),
 fields as (
-	select 
-		SAFE_CAST(campaign.name AS STRING) campaignName,
-		SAFE_CAST(report.impressions	AS	STRING)	impressions,
-		SAFE_CAST(report.clicks	AS	STRING)	clicks,
-		SAFE_CAST(report.campaign_budget_currency_code	AS STRING )	currency ,	
-		SAFE_CAST(report.campaign_budget_currency_code	AS STRING )	campaignBudgetCurrencyCode ,	
-		SAFE_CAST(report.units_sold_clicks_14_d as STRING) unitsSoldClicks14d,
-		SAFE_CAST(campaign.state	AS	STRING)	campaignStatus,
-		SAFE_CAST(profile.id AS STRING) profileId,
-		SAFE_CAST(profile.account_name as STRING) profileBrandName,
-		SAFE_CAST(campaign.id	AS	STRING)	campaignId,
-		SAFE_CAST(report.purchases_14_d	AS	STRING)	purchases14d,
-		SAFE_CAST(report.date	AS	DATE)	date,
-		SAFE_CAST(report.cost AS STRING) cost,
-		SAFE_CAST(report.sales_14_d	AS	STRING)	attributedSales14d,			
-		SAFE_CAST(profile.country_code	AS	STRING)	profileCountryCode
+       select
+                SAFE_CAST(campaigns.name AS STRING) campaignName,
+                SAFE_CAST(report.cost AS STRING) cost,
+                SAFE_CAST(report.impressions    AS      STRING) impressions,
+                SAFE_CAST(report.clicks AS      STRING) clicks,
+                SAFE_CAST(profile.currency_code       AS STRING )     currency ,      --using TRIM to get rid of trailing whitespace
+                SAFE_CAST(profile.account_name as STRING) profileBrandName,
+                SAFE_CAST(campaigns.id  AS      STRING) campaignId,
+                SAFE_CAST(profile.id AS STRING) profileId,
+                SAFE_CAST(report.date    AS      DATE)   date,
+                SAFE_CAST(profile.country_code  AS      STRING) profileCountryCode,
+                SAFE_CAST(campaigns.state AS    STRING) campaignStatus,
+                SAFE_CAST(report.campaign_budget_currency_code AS STRING) campaignBudgetCurrencyCode,
+                SAFE_CAST(report.purchases_clicks AS STRING) purchasesClicks,
+                SAFE_CAST(report.sales_clicks AS STRING) salesClicks
 		from report
 		left join campaign
 			on SAFE_CAST(campaign.id as INT64) = report.campaign_id and campaign.is_most_recent_record
