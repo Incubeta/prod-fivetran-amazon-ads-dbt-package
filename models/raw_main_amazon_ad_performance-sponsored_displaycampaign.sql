d196569831e41e81a973e2fe10a746905202af25
Miguelo Sana <miguelo@incubeta.com>
2024-12-06 10:34:43 +0100

    fix sponsored_displaycampaign

--- models/raw_main_amazon_ad_performance-sponsored_displaycampaign.sql
+++ models/raw_main_amazon_ad_performance-sponsored_displaycampaign.sql
@@ -44,7 +44,7 @@ fields as (
                 SAFE_CAST(campaigns.state AS    STRING) campaignStatus,
                 SAFE_CAST(report.campaign_budget_currency_code AS STRING) campaignBudgetCurrencyCode,
                 SAFE_CAST(report.purchases_clicks AS STRING) purchasesClicks,
-                SAFE_CAST(report.sales_clicks AS STRING) salesClicks
+                SAFE_CAST(report.sales_clicks AS STRING) salesClicks,
                 SAFE_CAST(null as STRING) attributedSales14d,
                 SAFE_CAST(null AS STRING) attributedConversions14d,
                 SAFE_CAST(null as STRING) attributedUnitsSold14d,
